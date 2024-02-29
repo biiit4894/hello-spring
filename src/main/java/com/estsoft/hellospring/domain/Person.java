68ddf9d99016e869b8f39c290dfa1a52b7d0c63e
Hanbit <ghiesf@naver.com>
2024-02-29 16:54:01 +0900

    Add: 롬복 적용(02.29)

--- src/main/java/com/estsoft/hellospring/domain/Person.java
+++ src/main/java/com/estsoft/hellospring/domain/Person.java
@@ -1,32 +1,16 @@
 package com.estsoft.hellospring.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
 public class Person {
     private String name;
     private int age;
-
-    public Person() {
-
-    }
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 
     @Override
     public String toString() {
