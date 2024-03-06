759e5ac0898e130385e6e0275568cb4c7cad1828
Hanbit <ghiesf@naver.com>
2024-03-06 15:45:27 +0900

    Feat: 학생 조회 및 등록 기능 구현

--- src/main/java/org/example/controller/HelloController.java
+++ src/main/java/org/example/controller/HelloController.java
@@ -1,15 +1,34 @@
 package org.example.controller;
 
+import org.example.repository.StudentJdbcRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
     // J2ME => mobile edition => android
     // J2SE => 지금 우리가 사용중인 java
     // J2EE => WEB 기반의 java version
+    @Autowired
+    private StudentJdbcRepository studentJdbcRepository;
+
     @GetMapping("/hello")
-    public String hi() {
-        return "Hello";
+    public int hi() {
+        return studentJdbcRepository.countOfStudent();
+    }
+
+    @GetMapping("/hello/name/list")
+    public List<String> getName() {
+        return studentJdbcRepository.getStudentNameList();
+    }
+
+    @PostMapping("/hello/name/regist")
+    public int saveStudent(@RequestParam String name, @RequestParam Integer age) {
+        return studentJdbcRepository.insertStudent(name, age);
     }
 }
---
+++ src/main/java/org/example/repository/StudentJdbcRepository.java
@@ -0,0 +1,28 @@
+package org.example.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class StudentJdbcRepository {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    public int countOfStudent() {
+        return jdbcTemplate.queryForObject("select count(1) from students", Integer.class);
+    }
+
+    public List<String> getStudentNameList() {
+        return jdbcTemplate.queryForList("select name from students", String.class);
+    }
+
+    public int insertStudent(String name, int age) {
+        return jdbcTemplate.update("insert into students(name, age) values (?, ?)", name, age);
+    }
+
+    // ID로 학생정보 조회하기
+
+    // student name or age or address 컬럼 정보 수정하기
+}
